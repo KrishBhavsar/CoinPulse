4a6788ca18b40af7b8efa0c3a6031eafdb448785
KrishBhavsar <bhavsarkrish308@gmail.com>
2026-01-03 16:13:21 +0530

    added coingecko apis

---
+++ components/home/CoinOverview.tsx
@@ -0,0 +1,38 @@
+import { fetcher } from "@/lib/coingecko.actions";
+import Image from "next/image";
+import { formatCurrency } from "@/lib/utils";
+import { CoinOverviewFallback } from "./fallback";
+
+const CoinOverview = async () => {
+  try {
+    const [coin] = await Promise.all([
+      fetcher<CoinDetailsData>("/coins/bitcoin", {
+        dex_pair_format: "symbol",
+      }),
+    ]);
+
+    return (
+      <div id="coin-overview">
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+            <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
+          </div>
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching coin overview:", error);
+    return <CoinOverviewFallback/>
+  }
+};
+
+export default CoinOverview;
