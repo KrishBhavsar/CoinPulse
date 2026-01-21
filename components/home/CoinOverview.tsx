5adb8b9a6d6933509fbf7f76c02c057a0e1476c4
KrishBhavsar <bhavsarkrish308@gmail.com>
2026-01-21 17:23:40 +0530

    fix home page

--- components/home/CoinOverview.tsx
+++ components/home/CoinOverview.tsx
@@ -1,8 +1,6 @@
 import { fetcher } from "@/lib/coingecko.actions";
-import Image from "next/image";
-import { formatCurrency } from "@/lib/utils";
 import { CoinOverviewFallback } from "./fallback";
-import CandlestickChart from "../CandlestickChart";
+import LiveCoinOverview from "./LiveCoinOverview";
 
 const CoinOverview = async () => {
   try {
@@ -16,26 +14,8 @@ const CoinOverview = async () => {
         precision: "full",
       }),
     ]);
-    return (
-      <div id="coin-overview">
-        <CandlestickChart data={coinOHLCData} coinId="bitcoin">
-          <div className="header pt-2">
-            <Image
-              src={coin.image.large}
-              alt={coin.name}
-              width={56}
-              height={56}
-            />
-            <div className="info">
-              <p>
-                {coin.name} / {coin.symbol.toUpperCase()}
-              </p>
-              <h1>{formatCurrency(coin.market_data.current_price.usd)}</h1>
-            </div>
-          </div>
-        </CandlestickChart>
-      </div>
-    );
+
+    return <LiveCoinOverview coin={coin} coinOHLCData={coinOHLCData} />;
   } catch (error) {
     console.error("Error fetching coin overview:", error);
     return <CoinOverviewFallback />;
---
+++ components/home/LiveCoinOverview.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { formatCurrency } from "@/lib/utils";
+import CandlestickChart from "../CandlestickChart";
+import useBinanceWebSocket from "@/hooks/useBinanceWebSocket";
+
+interface LiveCoinOverviewProps {
+  coin: CoinDetailsData;
+  coinOHLCData: OHLCData[];
+}
+
+const LiveCoinOverview = ({ coin, coinOHLCData }: LiveCoinOverviewProps) => {
+  const { ohlcv, price } = useBinanceWebSocket({
+    symbol: "btcusdt",
+    interval: "30m",
+  });
+
+  const displayPrice = price?.usd ?? coin.market_data.current_price.usd;
+
+  return (
+    <div id="coin-overview">
+      <CandlestickChart
+        data={coinOHLCData}
+        coinId="bitcoin"
+        liveOhlcv={ohlcv}
+        mode="live"
+        initialPeriod="daily"
+      >
+        <div className="header pt-2">
+          <Image
+            src={coin.image.large}
+            alt={coin.name}
+            width={56}
+            height={56}
+          />
+          <div className="info">
+            <p>
+              {coin.name} / {coin.symbol.toUpperCase()}
+            </p>
+            <h1>{formatCurrency(displayPrice)}</h1>
+          </div>
+        </div>
+      </CandlestickChart>
+    </div>
+  );
+};
+
+export default LiveCoinOverview;
